44e6cee7621e0228740f4d4a0bd867d54bb88dd1
Faseeh Naqvi <146260124+FNa4@users.noreply.github.com>
2023-11-14 00:10:18 +0000

    task1

--- StepsTask1.c
+++ StepsTask1.c
@@ -26,19 +26,41 @@ void tokeniseRecord(const char *input, const char *delimiter, char *date, char *
     }
 
     free(inputCopy);
+}
 
 int main () {
+
     FILE *fp;
 
     fp = fopen("FitnessData_2023.csv", "r");
     if (fp = NULL) {
-        printf("File Not Found");
+        printf("Error");
+        return 1;
     }
+    FITNESS_DATA lines [1000];
+     int read = 0;
+     int records = 0;
+    do {
+        read = fscanf(fp,"%c[],%c[],%d", lines[records].date, lines[records].time, &lines[records].steps);
+    
+        if (read == 4) {
+            records++;
+        }
 
-    fgets(FITNESS_DATA, 1000, fp);
+        else {
+            printf("Error\n");
+            return 1;
+        }
+    } while (!feof(fp));
+    
 
     fclose (fp);
 
-}
+    printf("Number of records in file: %d\n", records);
+
+    for (int i = 0; i < 3; i++) {
+        printf("%c %c %d\n", lines[i].date, lines[i].time, lines[i].steps);
+    }
 
 }
+
